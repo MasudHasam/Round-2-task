b47c75ded5fe319b235d1958d8d4eceba1a113bb
Masud Hasan <masudhasamf@gmail.com>
2022-11-30 16:20:45 +0600

    round-2 task done

---
+++ tasks.js
@@ -0,0 +1,71 @@
+const numArray = [2, 2, 2, 2, 2];
+let total = 0;
+const numArrayLength = numArray.length;
+numArray.map(numArrayValue => {
+    total = total + numArrayValue;
+});
+const splitter = total / numArrayLength;
+const output = total / splitter;
+console.log(output);
+
+
+const mailArray = [
+    'ghi@hotmail.com',
+    'def@yahoo.com',
+    'ghi@gmail.com',
+    'abc@channelier.com',
+    'abc@hotmail.com',
+    'def@hotmail.com',
+    'abc@gmail.com',
+    'abc@yahoo.com',
+    'def@channelier.com',
+    'jkl@hotmail.com',
+    'ghi@yahoo.com',
+    'def@gmail.com'];
+
+const userName = [];
+mailArray.map(value => {
+    const user = value.split('@');
+    userName.push(user);
+});
+userName.sort((a, b) => a[0].localeCompare(b[0]))
+
+const allUser = [];
+userName.map(user => {
+    const email = user.join('@');
+    allUser.push(email);
+});
+
+const sendMail = () => {
+    allUser.map(email => {
+        console.log(`Email send to ${email}`);
+    })
+}
+sendMail();
+
+
+const array = [1, 4, 3, 2];
+const largeNumber = []
+const smallerNumber = [];
+const largestNumber = Math.max(...array);
+array.map(element => {
+    if (element >= largestNumber) {
+        largeNumber.push(element)
+    };
+});
+const smallestNumber = Math.min(...array);
+array.map(element => {
+    if (element <= smallestNumber) {
+        smallerNumber.push(element)
+    };
+});
+
+const combined = [...largeNumber, ...smallerNumber];
+const divider = combined.length;
+let totalCombined = 0;
+combined.map(element => totalCombined = element + totalCombined);
+const avarage = totalCombined / divider;
+console.log(avarage);
+
+
+
